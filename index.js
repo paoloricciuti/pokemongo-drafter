8975c754494886e9f78e5e5c632ccfd2de9d9012
Paolo Ricciuti <ricciutipaolo@gmail.com>
2020-11-17 09:25:00 +0100

    Initial react app

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express=require("express");
+const mysql=require("mysql");
+const path = require('path');
+const dotenv=require("dotenv");
+
+dotenv.config();
+
 const app=express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res)=>{
-    res.json({
-        fuck: "you"
+const db= mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect((err)=>{
+    if(err){
+        throw err;
+    }
+    console.log("Mysql connected...");
+})
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get("/rooms", (req, res)=>{
+    db.query("SELECT * FROM rooms", (err, result) =>{
+        if(err){
+            throw err;
+        }
+        res.json(result);
     });
 })
 
